2e68929e16c6d7f057336cefc963eb3d875cbd78
Jenny Hou <jhou2012@gmail.com>
2021-01-18 23:26:56 -0500

    Minor grid and img formatting edits.

--- client/src/ReviewBlurb.jsx
+++ client/src/ReviewBlurb.jsx
@@ -23,13 +23,22 @@ class ReviewBlurb extends React.Component {
   render() {
     console.log('users test', this.props)
     return (
-      <div>
+      <div class="container-userReview">
         <p></p>
+
+        <div class="pic">
         <br/>{this.props.review.userId < 50 ? <img src={`https://randomuser.me/api/portraits/women/${this.props.review.userId}.jpg`}/> : <img src={`https://randomuser.me/api/portraits/men/${this.props.review.userId}.jpg`}/>}
-        <br/>{this.props.review.userId < 50 ? `https://randomuser.me/api/portraits/women/${this.props.review.userId}.jpg` : `https://randomuser.me/api/portraits/men/${this.props.review.userId}.jpg`}
+        {/* <br/>{this.props.review.userId < 50 ? `https://randomuser.me/api/portraits/women/${this.props.review.userId}.jpg` : `https://randomuser.me/api/portraits/men/${this.props.review.userId}.jpg`} */}
+        </div>
+
+        <div class="name">
         <br/>{this.props.users[this.props.review.userId]}
         <br/> {this.state.months[this.props.review.month]} {this.props.review.year}
+        </div>
+
+        <div class="desc">
         <br/>{this.props.review.description}
+        </div>
 
       </div>
     )
