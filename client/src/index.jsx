0ab83e77e4e8a9f12e828d5a4ea1c31ad67bb79e
Jenny Hou <jhou2012@gmail.com>
2021-01-18 22:47:05 -0500

    Formatted into basic grid layout, no font or image styling.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    axios('/propId/18')
+    axios('/propId/3')
     .then(res =>{
       console.log(res.data)
       let usersObj = {}
@@ -31,20 +31,43 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div class="container">
+
+        <div>
         {this.state.averages.avgOverall} ({this.state.totalReviews} reviews)
+        </div>
 
+        <div class="container-avgs">
+
+          <div>
         <br/>Cleanliness {this.state.averages.avgCleanliness}
+        </div>
+        <div>
         <br/>Accuracy {this.state.averages.avgAccuracy}
+        </div>
+        <div>
         <br/>Communication {this.state.averages.avgCommunication}
+        </div>
+        <div>
         <br/>Location {this.state.averages.avgLocation}
+        </div>
+        <div>
         <br/>Check-in {this.state.averages.avgCheckIn}
+        </div>
+        <div>
         <br/>Value {this.state.averages.avgValue}
+        </div>
 
+        </div>
+
+        <div class="container-reviews">
         {this.state.reviews6.map(review => <ReviewBlurb review={review} users={this.state.users} /> )}
+        </div>
 
+        <div>
         <p></p>
         <button>Show all {this.state.totalReviews} reviews</button>
+        </div>
 
       </div>
     )
