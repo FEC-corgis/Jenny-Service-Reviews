10485f961d50e6685fe72646835d84f2e9a460b1
Jenny Hou <jhou2012@gmail.com>
2021-01-21 13:39:26 -0500

    Refactor Averages.jsx to functional

--- client/src/Averages.jsx
+++ client/src/Averages.jsx
@@ -1,42 +1,34 @@
 import React from 'react';
 
-class Averages extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      test: '',
-    }
-  }
-  render() {
+const Averages = (props) => {
     // console.log('avgs props test', this.props)
-
-    if (!this.props.visible) {
+    if (!props.visible) {
       return (
         <div>
 
           <div class="overall">
-          {this.props.averages.avgOverall} ({this.props.reviewCount} reviews)
+          {props.averages.avgOverall} ({props.reviewCount} reviews)
           </div>
 
           <div class="container-avgs">
 
           <div>Cleanliness</div>
-            <div className="rating">{this.props.averages.avgCleanliness}</div>
+            <div className="rating">{props.averages.avgCleanliness}</div>
 
           <div>Accuracy</div>
-          <div className="rating">{this.props.averages.avgAccuracy}</div>
+          <div className="rating">{props.averages.avgAccuracy}</div>
 
           <div >Communication</div>
-          <div className="rating">{this.props.averages.avgCommunication}</div>
+          <div className="rating">{props.averages.avgCommunication}</div>
 
           <div>Location</div>
-          <div className="rating">{this.props.averages.avgLocation}</div>
+          <div className="rating">{props.averages.avgLocation}</div>
 
           <div>Check-in</div>
-          <div className="rating">{this.props.averages.avgCheckIn}</div>
+          <div className="rating">{props.averages.avgCheckIn}</div>
 
           <div>Value</div>
-          <div className="rating">{this.props.averages.avgValue}</div>
+          <div className="rating">{props.averages.avgValue}</div>
 
           </div>
 
@@ -47,35 +39,34 @@ class Averages extends React.Component {
         <div>
 
           <div class="overall">
-          {this.props.averages.avgOverall} ({this.props.reviewCount} reviews)
+          {props.averages.avgOverall} ({props.reviewCount} reviews)
           </div>
 
           <div class="modal-container-avgs">
 
           <div>Cleanliness</div>
-            <div className="rating">{this.props.averages.avgCleanliness}</div>
+            <div className="rating">{props.averages.avgCleanliness}</div>
 
           <div>Accuracy</div>
-          <div className="rating">{this.props.averages.avgAccuracy}</div>
+          <div className="rating">{props.averages.avgAccuracy}</div>
 
           <div >Communication</div>
-          <div className="rating">{this.props.averages.avgCommunication}</div>
+          <div className="rating">{props.averages.avgCommunication}</div>
 
           <div>Location</div>
-          <div className="rating">{this.props.averages.avgLocation}</div>
+          <div className="rating">{props.averages.avgLocation}</div>
 
           <div>Check-in</div>
-          <div className="rating">{this.props.averages.avgCheckIn}</div>
+          <div className="rating">{props.averages.avgCheckIn}</div>
 
           <div>Value</div>
-          <div className="rating">{this.props.averages.avgValue}</div>
+          <div className="rating">{props.averages.avgValue}</div>
 
           </div>
 
         </div>
       )
     }
-  }
 }
 
 export default Averages;
