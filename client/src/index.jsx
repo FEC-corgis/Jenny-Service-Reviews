d41aab6e242093fa107c14d041d981dde04c587b
Jenny Hou <jhou2012@gmail.com>
2021-01-21 12:14:34 -0500

    Refactored App component to react hooks.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,29 +1,22 @@
-import React from 'react';
+import React, { setState, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Averages from './Averages.jsx'
 import ReviewBlurb from './ReviewBlurb.jsx'
 import Modal from './Modal.jsx'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      averages: '',
-      reviews6: [],
-      reviewsAll: [],
-      users: {},
-      reviewCount: '',
-      modal: false,
-    }
-    this.toggleModal = this.toggleModal.bind(this)
+const App = () => {
+  const [averages, setAverages] = useState('')
+  const [reviews6, setReviews6] = useState([])
+  const [reviewsAll, setReviewsAll] = useState([])
+  const [users, setUsers] = useState({})
+  const [reviewCount, setReviewCount] = useState('')
+  const [modal, setModal] = useState(false)
+
+  const toggleModal = () => {
+    setModal(!modal)
   }
-  toggleModal() {
-    this.setState({
-      modal: !this.state.modal
-    })
-  }
-  componentDidMount() {
+  useEffect(() => { // is useEffect equiv to componentDidMount?
     axios('/propId/7')
     .then(res =>{
       console.log(res.data)
@@ -35,49 +28,45 @@ class App extends React.Component {
       for (let i = 0; i < 6; i++) {
         top6.push(res.data.reviews[i])
       }
-      this.setState({
-        averages: res.data.averages,
-        reviews6: top6,
-        reviewsAll: res.data.reviews,
-        users: usersObj,
-        reviewCount: res.data.reviewCount,
-      })
+      setAverages(res.data.averages)
+      setReviews6(top6)
+      setReviewsAll(res.data.reviews)
+      setUsers(usersObj)
+      setReviewCount(res.data.reviewCount)
     })
-  }
-  render() {
+  }, [])
     return (
       <div class="container">
 
         <div>
         <p></p>
-        <button onClick={this.toggleModal}>Show all {this.state.reviewCount} reviews</button>
+        <button onClick={toggleModal}>Show all {reviewCount} reviews</button>
         <Modal
-        visible={this.state.modal}
-        toggleModal={this.toggleModal}
-        averages={this.state.averages}
-        reviewCount={this.state.reviewCount}
-        reviewsAll={this.state.reviewsAll}
-        users={this.state.users}/>
+        visible={modal}
+        toggleModal={toggleModal}
+        averages={averages}
+        reviewCount={reviewCount}
+        reviewsAll={reviewsAll}
+        users={users}/>
         </div>
 
-          <p></p>
-          <div>
-          <Averages
-          averages={this.state.averages}
-          reviewCount={this.state.reviewCount} />
-          </div>
+        <p></p>
+        <div>
+        <Averages
+        averages={averages}
+        reviewCount={reviewCount} />
+        </div>
 
         <div class="container-reviews">
-        {this.state.reviews6.map(review =>
+        {reviews6.map(review =>
         <ReviewBlurb
         review={review}
-        users={this.state.users}
+        users={users}
         /> )}
         </div>
 
       </div>
     )
-  }
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'))
