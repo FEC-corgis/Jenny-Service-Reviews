b1ecc7ac9a5002c7f344aa13db36ddc3a9bc123b
Jenny Hou <jhou2012@gmail.com>
2021-01-21 13:06:45 -0500

    Refactor Modal.jsx back to functional

--- client/src/Modal.jsx
+++ client/src/Modal.jsx
@@ -2,33 +2,25 @@ import React from 'react';
 import Averages from './Averages.jsx';
 import ReviewBlurb from './ReviewBlurb.jsx';
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      test: '',
-    }
-    this.onClose = this.onClose.bind(this)
+const Modal = (props) => {
+  const onClose = () => {
+    props.toggleModal()
   }
-  onClose() {
-    this.props.toggleModal()
-  }
-  render() {
-   console.log('reviews test', this.props)
-    if (!this.props.visible) {
+  //  console.log('reviews test', this.props)
+    if (!props.visible) {
       return null
     } else {
       return (
         <div className="modalBackground">
           <div className="modalBody">
 
-          <div className="modal-X" onClick={this.onClose}>X</div>
+          <div className="modal-X" onClick={onClose}>X</div>
 
               <div className="modal-avgs">
               <Averages
-              averages={this.props.averages}
-              totalReviews={this.props.totalReviews}
-              visible={this.props.visible}/>
+              averages={props.averages}
+              totalReviews={props.totalReviews}
+              visible={props.visible}/>
               </div>
 
               <div className="modal-search">
@@ -40,15 +32,14 @@ class Modal extends React.Component {
               </div>
 
               <div className="modal-reviews">
-              {this.props.visible ?
-              this.props.reviewsAll.map(review => <ReviewBlurb review={review} users={this.props.users} />) :
-              this.props.reviews6.map(review => <ReviewBlurb review={review} users={this.props.users} />)}
+              {props.visible ?
+              props.reviewsAll.map(review => <ReviewBlurb review={review} users={props.users} />) :
+              props.reviews6.map(review => <ReviewBlurb review={review} users={props.users} />)}
               </div>
 
             </div>
         </div>
       )
-    }
     }
 }
 
