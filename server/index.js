4a2639bf713852ee9807b8e77329a5041c531e90
Jenny Hou <jhou2012@gmail.com>
2021-02-27 20:46:56 -0500

    compress test

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(compression())
+app.use(express.compress())
 
 app.get('/reviews/propId/:id', async (req, res) => {
   const id = req.params.id
